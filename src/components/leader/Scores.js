a21d64392a15d6f947ccfb129cc14d2eeec1937e
UncleJerry <jared.myhrberg@gmail.com>
2019-12-09 11:23:09 -0800

    scroll to new score working

--- src/components/leader/Scores.js
+++ src/components/leader/Scores.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Score from './Score';
 import styles from './scores.css';
 
 const Scores = ({ scores, codeName, newScore }) => {
+
+  useEffect(() => {
+    const currentScore = document.querySelector(`.${styles.NewGame}`);
+    if(currentScore) currentScore.scrollIntoView('alignTo');
+  }, []);
+
   const scoreList = scores.map(({ name, score }, i) => {
     let newGame = '';
-    if(codeName === name && newScore == score) newGame = styles.NewGame;
+    if(codeName === name && newScore == score) {
+      newGame = styles.NewGame;
+      // setCurrentScore(true);
+    }
 
     return (
-      <li key={`${name}-${score}`} className={newGame}>
+      <li key={`${name}-${score}-${i}`} className={newGame}>
         <Score name={name} score={score} rank={i}/>
       </li>
     );
   });
+
   return (
     <div className={styles.Scores}>
       <header>
